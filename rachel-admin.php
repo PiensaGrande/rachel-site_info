31cf9c6169e0e9b00a775f16f3e5800aecd3a519
Fred Murphy <fmurphy@piensagrande.org>
2017-12-02 18:19:06 -0600

    Fix type in jquery hints and normalize comments

--- rachel-admin.php
+++ rachel-admin.php
@@ -1,14 +1,15 @@
 <?php namespace pg_site_info; ?>
 <?php require_once($_SERVER["DOCUMENT_ROOT"] .  "/admin/common.php"); ?>
-<?php include "template.php";?>
-<!-- This is a rachel-admin.php file. It is an optional file that RACHEL uses to
-     display your module on the RACHEL admin page to allow access to admin-specific files or functions. 
-     You should place this file in your module's directory if needed.
-     As it uses the template.php file, you should be able to set your specific info there.
--->
+<?php 
+include "template.php";
 
-<div class='adminmodule' data-moduletype='{$templ['module_type']}' data-title='{templ['title']}' data-img_uri='{$templ['img_uri']}' data-index_loc='{$templ['index_loc']}'>
-<?php
+// This is a rachel-admin.php file. It is an optional file that RACHEL uses to
+//     display your module on the RACHEL admin page to allow access to admin-specific files or functions. 
+//     You should place this file in your module's directory if needed.
+//     As it uses the template.php file, you should be able to set your specific info there.
+
+echo "<div class='adminmodule' data-moduletype='{$templ['module_type']}' data-title='{$templ['title']}' data-img_uri='{$templ['img_uri']}' data-index_loc='{$templ['index_loc']}'>";
+
 // Place your admin code here.
 
 // We use an engine which processes a form in the background if called from a POST and then
